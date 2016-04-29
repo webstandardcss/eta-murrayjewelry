44bb2815768efd07649997a53b88820fb6543c68
Trey Brister <webstandardcss@gmail.com>
2016-04-29 15:38:44 -0500

    html changes

--- layaway.php
+++ layaway.php
@@ -7,18 +7,13 @@
 			<h2 class="text-center">Layaway</h2>
 			<h1 class="text-center">OUR LAYAWAY SERVICES</h1>
 			
-			<p>Murray Jewelry offers Layaway for In-Store Merchandise.<br />
-We make it easy for you to get that special item.</p>
+			<p>Murray Jewelry offers Layaway for In-Store Merchandise. We make it easy for you to get that special item.</p>
 
-			<p>Layaway an item for up to One Year<br />
-Pay only 20% Down<br />
-With 10% Payment per Month</p>
+			<p>Layaway an item for up to One Year; Pay only 20% Down With 10% Payment per Month</p>
 			
-			<p>* Sorry, No Cash Refunds on Layaways.</p>
+			<ul><li>Sorry, No Cash Refunds on Layaways.</li></ul>
 			
-			<p>Don&rsquo;t Know What to Get??<br />
-Murray Jewelry also offers Gift Certificates.<br />
-You Spend What You Want and They Select Exactly the Gift They Want!</p>
+			<p>Don&rsquo;t Know What to Get? Murray Jewelry also offers Gift Certificates. You Spend What You Want and They Select Exactly the Gift They Want!</p>
 			
 			
 		<!-- Main Contents Ends -->
