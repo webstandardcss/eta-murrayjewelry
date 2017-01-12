db11e4a6443455442753f4f149aa8e2f0896f322
Trey Brister <webstandardcss@gmail.com>
2017-01-11 23:57:27 -0600

    million changes why was this site never updated before now

---
+++ bellarri.php
@@ -0,0 +1,18 @@
+<?php include_once('header.php'); ?>
+		
+		<section class="main-contents container">
+		<div class="3">
+		<!-- Main Contents Starts -->
+		
+			<h2 class="text-center">Bellarri</h2>
+			
+			<img src="images/Bellari.jpg-large.jpg" class="img-responsive img-fix" style="width: 300px" />
+			<br />			<p>Trend setting design and quality workmanship make Bellarri an industry leader. She believes "that every piece in my collections should be the epitome of elegance and heirloom quality." Her distinctive collections mark her as a designer of &ldquo;Art&rdquo;, not simply &ldquo;beautiful baubles.&rdquo;</p>			
+			
+			<p>Romance and dreams are alive and well in this designer, as Bellarri thoughtfully states, "If my designs elicit images of romance and dreams of far away places, perhaps it is because these have always been a part of my life. I am the direct result of a woman following her heart."</p>
+			
+		<!-- Main Contents Ends -->
+		</div>
+		</section>
+		
+<?php include_once('footer.php'); ?>
---
+++ unavelo-bridal-line.php
@@ -0,0 +1,36 @@
+<?php include_once('header.php'); ?>
+		
+		<section class="main-contents container">
+		<div class="3">
+		<!-- Main Contents Starts -->
+					
+			<div class="text-center" style="margin-top:3em;margin-bottom:3em;">
+			<picture>
+			<source srcset="/images/logo-unavelo.png" media="(max-width: 400px)">
+			<source srcset="/images/logo.png">
+			<a href="http://www.unavelo.com/">
+			<img src="/images/logo-unavelo.png" alt="Unavelo Logo" style="width:auto;">
+			</a>
+			</div>
+			
+			<p style="margin-bottom:3em;">The intimacy shared between you and your loved one is truly unique. UNAVELO honors this with uniquely differentiated designs that encapsulate the private meanings from within that are meant to endure. Each couple&rsquo;s journey is truly theirs. Our rings are yours alone, symbols between you and your chosen, an oath between you two.</p>
+			
+			<div class="text-center">
+			<picture>
+			<source srcset="images/unavelo-ring.png" media="(max-width: 320px)">
+			<source srcset="/images/unavelo-ring.png">
+			<img src="/images/unavelo-ring.png" alt="Unavelo Ring" style="width:auto;">
+			</picture>
+			</div>
+			<div class="text-center" style="margin-top:5em;">
+			
+			<img src="/images/pink-diamond-logo.png" class="img-responsive img-fix" alt="Pink Diamond Logo" style="width:auto;">
+			
+			<p style="margin-bottom:3em; padding:0 50px 0 50px">Unavelo has chosen to place a rare pink diamond on the inside of every ring because there is no more powerful symbol of the rare, valuable, and unique love that you share with your spouse. It is held on the inside, out of view, veiled from the world, just as your love for your spouse is held inside your heart. Our pink diamond is your reminder that what's on the inside matters most.</p>
+			</div>
+			
+		<!-- Main Contents Ends -->
+		</div>
+		</section>
+		
+<?php include_once('footer.php'); ?>
