030e72bef9a06d28000f88427c8689f813e7d113
Trey Brister <webstandardcss@gmail.com>
2017-04-21 13:55:59 -0500

    secure image thumbnail library

---
+++ tests/tests.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * tests.php - basic usage tests for mThumb.
+ *
+ * Images are from Wikimedia Commons Public Domain category.
+ *
+ * @created   11/12/14 10:26 PM
+ * @author    Mindshare Studios, Inc.
+ * @copyright Copyright (c) 2014
+ * @link      http://www.mindsharelabs.com/documentation/
+ *
+ */
+?>
+<p>
+	// test jpg<br /> <img src="/tn.php?src=/tests/small.jpg&w=100&h=100" alt="test jpg" />
+</p>
+<p>
+	// test png<br /> <img src="/tn.php?src=/tests/medium.png&w=500&h=600" alt="test png" />
+</p>
+<p>
+	// test gif<br /> <img src="/tn.php?src=/tests/medium.gif&w=150&h=160" alt="test gif" />
+</p>
+<p>
+	// test external<br /> <img src="/tn.php?src=https://farm8.staticflickr.com/7559/15590345589_8e79f8744d_k.jpg&w=500&h=300" alt="test external jpg" /><br />
+	// test external<br />
+	<img src="/tn.php?src=https://i.imgur.com/g5kZOtT.png&w=500&h=300" alt="test external jpg" />
+</p>
+<p>
+	// test large file size<br /> <img src="/tn.php?src=/tests/large.jpg&w=100&h=100" alt="test jpg" />
+</p>
+<p>
+	// test tilde<br /> <img src="/tn.php?src=/tests/small.jpg&w=100&h=100" alt="test jpg" />
+</p>
