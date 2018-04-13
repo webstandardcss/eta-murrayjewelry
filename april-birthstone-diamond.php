6b2f98a5514f7977fc31b1c87be656fbd2558635
Trey Brister <webstandardcss@gmail.com>
2018-04-13 17:12:19 -0500

    april birthstone diamonds page

---
+++ april-birthstone-diamond.php
@@ -0,0 +1,55 @@
+<?php include_once('header.php'); ?><script>fbq('track', 'ViewContent', {value: 3.50,currency: 'USD'});</script>
+		
+		<section class="main-contents container">
+		<div class="3">
+		<!-- Main Contents Starts -->
+			
+			<!-- div class="text-center">
+				<img src="images/reactor-watches-st-louis.jpg" class="img-responsive img-fix" />
+			</div -->
+		
+			<h2 class="text-center">April's Birthstone</h2>
+			<h1 class="text-center">Diamond</h1>
+			<!-- Gallery  example
+			<a rel="gallery-murray" href="big/image1.jpg" class="galleryimage">
+				<img src="small/image1.jpg" alt="image">
+			</a>
+			-->
+	<p>Every month's birthstone is special, and for those fortunate to be born in April, the most prized gemstone of all is their birthstone. For this month, diamonds truly are a girl’s (or a boy’s) best friend.</p>
+	<p>Here is a little more information on this gorgeous stone.</p>
+	<p>You probably already know of the diamond’s toughness. In fact, it’s the hardest gemstone and is made of just one element: carbon.</p>
+	<p>Diamonds come in several colors, including yellow, red, pink, blue, and green, and range in intensity from faint to vivid. Generally speaking, the more saturated the color, the higher the value.</p>
+	<p>While it’s become nearly synonymous with wedding engagements, it’s also the perfect stone for individuals who want something that’s just as appropriate for everyday wear as it is for special occasions.</p>
+	<p>Its unique physical properties means it has the best possible luster of any gemstone when cut and polished well. So if you’re in the market for “sparkle,” the diamond is the gemstone for you.</p>
+	
+<div class="gallery gallery-murray">
+	<h3>Murray Diamond Jewelry</h3>
+	<p>
+	<a rel="gallery-murray" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07784-edited_murray_jewelry.jpg.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07784-edited_murray_jewelry.jpg&h=200" /></a>
+	<a rel="gallery-murray" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07788-edited_murray_jewelry.jpg.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07788-edited_murray_jewelry.jpg&h=200" /></a>
+	<a rel="gallery-murray" class="galleryimage" href="/tn.php?src=/images/gallery/dsc00406-edited_murray_jewelry.jpg.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc00406-edited_murray.jpg&h=200" /></a>
+	<a rel="gallery-murray" class="galleryimage" href="/tn.php?src=/images/gallery/dsc00423-edited_murray.jpg.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc00423-edited_murray.jpg&h=200" /></a>
+	<a rel="gallery-murray" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07747-edited_murray.jpg.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07747-edited_murray_jewelry.jpg&h=200" /></a>
+	</p>
+</div>
+
+<div class="gallery gallery-used">
+	<h3>Jewelry</h3>
+	<p>
+	<a rel="gallery-used" class="galleryimage" href="/tn.php?src=/images/gallery/dsc00331-edited_used.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc00331-edited_used.jpg&h=200" /></a>
+	<a rel="gallery-used" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07706-edited_used.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07706-edited_used.jpg&h=200" /></a>
+	<a rel="gallery-used" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07722-edited_used.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07722-edited_used.jpg&h=200" /></a>
+	<a rel="gallery-used" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07728-edited_used.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07728-edited_used.jpg&h=200" /></a>
+	<a rel="gallery-used" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07730-edited_used.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07730-edited_used.jpg&h=200" /></a>
+	<a rel="gallery-used" class="galleryimage" href="/tn.php?src=/images/gallery/dsc07754-edited_used.jpg&w=800"><img alt="" src="/tn.php?src=/images/gallery/dsc07754-edited_used.jpg&h=200" /></a>
+	</p>
+</div>
+
+
+			<!--img src="images/oxide_34.jpg" class="img-responsive img-fix" /-->
+			
+		<!-- Main Contents Ends -->
+		</div>
+		</section>
+		
+<?php include_once('footer.php'); ?>
